bc243d8ebddce8ceb32df266e0fdcd4bf0ce7fb0
MKC22 <marie@cc-bs.de>
2015-01-28 21:56:10 +0100

    Kommentare ImprintController

--- app/src/org/dhbw/imbit11/ImprintController.java
+++ app/src/org/dhbw/imbit11/ImprintController.java
@@ -11,17 +11,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Class contains the doGet and doPost methods for responding with the content of the Frontend
+ * "Impressum" to a clients response
+ * 
+ * @author Mary
+ *
+ *@version 2.0
+ */
+
 @WebServlet({ "/Imprint" })
 public class ImprintController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private RequestDispatcher jsp;
 	
+	/*
+	 * (non-Javadoc)
+	 * Information of the "Impressum" brillianCRM is saved to the Object jsp after Servlet config
+	 * is placed into service and Servlet Context Object created
+	 * @author Mary
+	 */
 	public void init(ServletConfig config) throws ServletException {
 		ServletContext context = config.getServletContext();
 		jsp = context.getRequestDispatcher("/frontend/imprint.jsp");
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * Method allows Servlet to handle a get request
+	 * Format of the response is HTML with UTF-8 Encoding
+	 * Server-side information of the About brillianCRM is saved to response
+	 * @param {object} request - contains the request of a client
+	 * @param {object} response - contains the response of the servlet
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html");
@@ -30,6 +54,15 @@ public class ImprintController extends HttpServlet {
 		jsp.include(request, response);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * Method allows servlet to handle post requests, so client can send data
+	 * of unlimited length to web server
+	 * Server-side information of the About brillianCRM is saved to response
+	 * @param {object} request - contains the request of a client
+	 * @param {object} response - contains the response of the servlet
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html");
