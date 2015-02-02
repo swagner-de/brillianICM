139c15cdef7ba0400d79219212421410822da44a
sceiler <sceiler14@gmail.com>
2015-02-02 11:34:09 +0100

    add temp buttons, load xml and show only id attribute

--- GameEditor/GameEditor/Form1.Designer.cs
+++ GameEditor/GameEditor/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GameEditor
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadXMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnTempLoadXml = new System.Windows.Forms.Button();
+            this.btnTempExit = new System.Windows.Forms.Button();
+            this.txtBoxOut = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1026, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadXMLToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadXMLToolStripMenuItem
+            // 
+            this.loadXMLToolStripMenuItem.Name = "loadXMLToolStripMenuItem";
+            this.loadXMLToolStripMenuItem.Size = new System.Drawing.Size(198, 30);
+            this.loadXMLToolStripMenuItem.Text = "Load XML";
+            this.loadXMLToolStripMenuItem.Click += new System.EventHandler(this.loadXMLToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(195, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(198, 30);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(74, 29);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // btnTempLoadXml
+            // 
+            this.btnTempLoadXml.Location = new System.Drawing.Point(12, 63);
+            this.btnTempLoadXml.Name = "btnTempLoadXml";
+            this.btnTempLoadXml.Size = new System.Drawing.Size(169, 102);
+            this.btnTempLoadXml.TabIndex = 1;
+            this.btnTempLoadXml.Text = "button1";
+            this.btnTempLoadXml.UseVisualStyleBackColor = true;
+            this.btnTempLoadXml.Click += new System.EventHandler(this.btnTempLoadXml_Click);
+            // 
+            // btnTempExit
+            // 
+            this.btnTempExit.Location = new System.Drawing.Point(197, 63);
+            this.btnTempExit.Name = "btnTempExit";
+            this.btnTempExit.Size = new System.Drawing.Size(163, 102);
+            this.btnTempExit.TabIndex = 2;
+            this.btnTempExit.Text = "button2";
+            this.btnTempExit.UseVisualStyleBackColor = true;
+            this.btnTempExit.Click += new System.EventHandler(this.btnTempExit_Click);
+            // 
+            // txtBoxOut
+            // 
+            this.txtBoxOut.Location = new System.Drawing.Point(387, 63);
+            this.txtBoxOut.Multiline = true;
+            this.txtBoxOut.Name = "txtBoxOut";
+            this.txtBoxOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtBoxOut.Size = new System.Drawing.Size(627, 573);
+            this.txtBoxOut.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1026, 712);
+            this.Controls.Add(this.txtBoxOut);
+            this.Controls.Add(this.btnTempExit);
+            this.Controls.Add(this.btnTempLoadXml);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "Game Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadXMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button btnTempLoadXml;
+        private System.Windows.Forms.Button btnTempExit;
+        private System.Windows.Forms.TextBox txtBoxOut;
     }
 }
 
--- GameEditor/GameEditor/Form1.cs
+++ GameEditor/GameEditor/Form1.cs
@@ -7,14 +7,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
 
 namespace GameEditor
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void loadXMLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "XML|*.xml";
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            ofd.FileName = "masterfile.xml";
+            ofd.Title = "Load XML file";
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                XElement rootElement = XElement.Load(ofd.FileName);
+                txtBoxOut.Text = GetOutline(0, rootElement);
+            }
+            else
+            {
+                // Nah do nothing..
+            }
+        }
+
+        private void btnTempExit_Click(object sender, EventArgs e)
+        {
+            quitToolStripMenuItem_Click(sender, e);
+        }
+
+        private void btnTempLoadXml_Click(object sender, EventArgs e)
+        {
+            loadXMLToolStripMenuItem_Click(sender, e);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Work in progress");
+        }
+
+        private string GetOutline(int indentLevel, XElement element)
+        {
+            StringBuilder result = new StringBuilder();
+
+            if (element.Attribute("id") != null)
+            {
+                result = result.AppendLine(new string(' ', indentLevel * 2) + element.Attribute("id").Value);
+            }
+
+            foreach (XElement childElement in element.Elements())
+            {
+                result.Append(GetOutline(indentLevel + 1, childElement));
+            }
+
+            return result.ToString();
         }
     }
 }
