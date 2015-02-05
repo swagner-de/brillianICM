c025aab2688932136eb68cf009178455900b628e
oibchr <oliver.becher@gmail.com>
2015-02-05 10:17:15 +0100

    Hotfix

--- app/src/org/dhbw/imbit11/backend/ChangeStudentPassword.java
+++ app/src/org/dhbw/imbit11/backend/ChangeStudentPassword.java
@@ -20,8 +20,8 @@ import javax.servlet.annotation.WebServlet;
    static final long serialVersionUID = 1L;
  	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url="/backend/homepage_student.jsp";
 		doPost(request, response);
-		  String url="/backend/homepage_student.jsp";
 	}  	
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
