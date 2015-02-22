6ba02963b1a516f6873ccf035958f1b86a69f198
Sunacni <frederik@diebrauns.eu>
2015-02-22 20:16:56 +0100

    regarding issue # 291

--- app/src/org/dhbw/imbit11/backend/SendRegistrationLink.java
+++ app/src/org/dhbw/imbit11/backend/SendRegistrationLink.java
@@ -57,21 +57,23 @@ import javax.servlet.annotation.WebServlet;
 		String toMail = request.getParameter("email");
 		String username = (String) request.getAttribute("username");		
 		//Test Email mit HTML tags
-		String content="<table cellspacing='0' style='width: 570.0px;'>";
-		content+="<tr><td style='padding: 15.0px 0 15.0px 0;'><img alt='brillianCRM logo' src='../../WebContent/images/logo.png'/></td>";
-		content+="<td align='right' style='padding: 15.0px 0 15.0px 0; font-family: arial; font-size: 12.0px;'><a href='https://brilliancrm.com/' target='_blank' style='color:#000000; text-decoration:none'>brilliancrm.com</a></td></tr>";
-		content+="<tr><td colspan='2' style='padding: 15.0px; background-color: #c1d3f6; font-family: arial; font-size: 14.5px; font-weight: bold;'>brillianCRM registration link</td></tr>";
-		content+="<tr><td colspan='2' style='padding: 14.0px; font-family: arial; font-size: 14.0px;'>";
-		content+="<div><p>Dear Project Manager,</p>";
-		content+="<p>Welcome to brillianCRM, the serious game for Project Management. This game is designed by students, for students. Please <a href='" + link + "' style='color:#6699ff; text-decoration:none'>click here</a> and register to your course.</p><br>";
-		content+="<p>With best regards,</p><br>";
-		content+="<p>the brillianCRM Team</p></div>";
-		content+="<div style='font-size: 12.0px; color:#707a8f; font-family: Arial;'><br>";
-		content+="<p>Please notify that this is a system generated email. Please do not reply.</p><br>";
-		content+="Studiengang IMBIT, DHBW Mannheim, Prof. Dr. Harald Bendl<br>";
-		content+="Coblitzallee 1 - 9, D-68163 Mannheim<br>";
-		content+="Tel. +49 621 4105-1719, Fax +49 621 4105-1289, harald-bendl@dhbw-mannheim.de, www.IMBIT.dhbw-mannheim.de<br></div></td></tr>";
-		content+="<tr><td colspan='2'><img alt='' src='../../WebContent/images/emailBottomLine.gif'/></td></tr></table>";
+		String picture1Path = application.getContextPath()+"/images/logo.png";
+		String picture2Path = application.getContextPath()+"/images/emailBottomLine.png";
+		String content="<table cellspacing='0' style='width: 570.0px;'>"
+		+ "<tr><td style='padding: 15.0px 0 15.0px 0;'><img alt='brillianCRM logo' src='" + picture1Path + "'/></td>";
+		+"<td align='right' style='padding: 15.0px 0 15.0px 0; font-family: arial; font-size: 12.0px;'><a href='https://brilliancrm.com/' target='_blank' style='color:#000000; text-decoration:none'>brilliancrm.com</a></td></tr>"
+		+"<tr><td colspan='2' style='padding: 15.0px; background-color: #c1d3f6; font-family: arial; font-size: 14.5px; font-weight: bold;'>brillianCRM registration link</td></tr>"
+		+"<tr><td colspan='2' style='padding: 14.0px; font-family: arial; font-size: 14.0px;'>"
+		+"<div><p>Dear Project Manager,</p>"
+		+"<p>Welcome to brillianCRM, the serious game for Project Management. This game is designed by students, for students. Please <a href='" + link + "' style='color:#6699ff; text-decoration:none'>click here</a> and register to your course.</p><br>"
+		+"<p>With best regards,</p><br>"
+		+"<p>the brillianCRM Team</p></div>"
+		+"<div style='font-size: 12.0px; color:#707a8f; font-family: Arial;'><br>"
+		+"<p>Please notify that this is a system generated email. Please do not reply.</p><br>"
+		+"Studiengang IMBIT, DHBW Mannheim, Prof. Dr. Harald Bendl<br>"
+		+"Coblitzallee 1 - 9, D-68163 Mannheim<br>"
+		+"Tel. +49 621 4105-1719, Fax +49 621 4105-1289, harald-bendl@dhbw-mannheim.de, www.IMBIT.dhbw-mannheim.de<br></div></td></tr>"
+		+"<tr><td colspan='2'><img alt='' src='" + picture2Path + "'/></td></tr></table>";
 		
 		//***Original Email without using HTML
 		//String content = "Please use the following link to register to your course: " + link + "\n\nGreetings, \n" +
